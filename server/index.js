2ef6b8602c6a5586412e091a24161143fb673e99
anasparambadan <forandro002@gmail.com>
2024-01-30 23:12:26 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import salesRoute from "./routes/sales.js";
+import managementRoutes from "./routes/mangement.js";
+
+// data import
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import {dataUser,dataProduct,dataProductStat} from './data/index.js'
+
+// configuraton
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoute);
+
+// mongoose setup
+
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server connected to PORT : ${PORT}`));
+    // User.insertMany(dataUser)
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
+  }).catch((error)=>console.log(`${error}, Database onnection failed`));
